d29ce2e64d5be143aacaad3c6fd09d6b144b1775
Cloudef <mailRoxas@gmail.com>
2011-09-15 20:07:50 +0300

    Some basic shader code (forgot the files)

---
+++ test/test_shader/shader.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <SDL/SDL.h>
+
+#include "ogl/ogl.h"
+#include "ogl/window.h"
+#include "input.h"
+
+static void keyHandle( void )
+{
+   SDL_Event event;
+
+   while(SDL_PollEvent(&event))
+   {
+      switch (event.type) {
+         case SDL_KEYDOWN:
+            keyAdd(event.key.keysym.sym);
+         break;  /* SDL_KEYDOWN */
+         case SDL_KEYUP:
+            keyDel(event.key.keysym.sym);
+         break;  /* SDL_KEYUP */
+         case SDL_VIDEOEXPOSE:
+         break;  /* SDL_VIDEOEXPOSE */
+         case SDL_VIDEORESIZE:
+         break;  /* SDL_VIDEORESIZE */
+         case SDL_QUIT:
+            keyAdd(SDLK_ESCAPE);
+         break;  /* SDL_QUIT */
+      }
+   }
+}
+
+static void cleanup( int ret )
+{
+   glFreeDisplay();
+   glCloseWindow();
+   SDL_Quit();
+
+   /* exit graph */
+   glMemoryGraph();
+
+   exit( ret );
+}
+
+int main( int argc, char **argv )
+{
+   glShader *shader;
+
+   unsigned int   flags = SDL_OPENGL | SDL_RESIZABLE;
+   int            width = 800;
+   int            height= 480;
+   int            bits  = 32;
+#if defined(GLES1) || defined(GLES2)
+   flags = SDL_RESIZABLE;
+   width = 800;
+   height= 480;
+   bits  = 16;
+#endif
+
+   if(SDL_Init(   SDL_INIT_VIDEO    ) != 0)
+      cleanup(EXIT_FAILURE);
+
+   if(glCreateWindow( width, height, bits, flags ) != 0)
+      cleanup(EXIT_FAILURE);
+
+   if(glCreateDisplay( width, height, GL_RENDER_DEFAULT ) != 0)
+      cleanup(EXIT_FAILURE);
+
+   /* create shader */
+   shader = glNewShader( "shader/ogl3.shd" );
+
+   /* startup graph */
+   glMemoryGraph();
+
+   /* wait for escape key */
+   while(!keyPress(SDLK_ESCAPE))
+   {
+      keyHandle();
+      glSwapBuffers();
+   }
+   glFreeShader( shader );
+
+   cleanup(EXIT_SUCCESS);
+   return(EXIT_SUCCESS);
+}
